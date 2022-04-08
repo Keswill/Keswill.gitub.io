8dfbaaa29a38879f3287c70b55a8d9ffb0d592e1
Keswill <65160798+Keswill@users.noreply.github.com>
2022-04-08 13:04:34 +0800

    ++a先加1，a++后加1

---
+++ 前加与后加的区别.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    int b = 20;
+    cout << ++b * a << endl;
+    cout << b++ * a << endl;
+
+    system("pause");
+
+    return 0;
+}
