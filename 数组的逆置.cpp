06532f24842bf33b5f90dc3145bc2a3e5e45465b
Keswill <65160798+Keswill@users.noreply.github.com>
2022-04-13 10:36:41 +0800

    实现数组逆置

---
+++ 数组的逆置.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5] = {1,3,2,5,4};
+    cout << "逆置前数组：" << endl;
+    for (int i = 0;i < 5;i++)
+    {
+        cout << arr[i];
+    }
+    int start = 0;
+    int end = sizeof(arr)/sizeof(arr[0])-1;
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+    cout << "\n逆置后数组：" << endl;
+    for (int i = 0;i < 5;i++)
+    {
+        cout << arr[i];
+    }
+}
